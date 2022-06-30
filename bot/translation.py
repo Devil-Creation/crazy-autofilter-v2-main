eea79b38db69b63a5250082822470f7dc2fd3ded
Devil-Creation <91359456+Devil-Creation@users.noreply.github.com>
2022-06-30 09:05:28 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -21,27 +21,27 @@ class Translation(object):
 
 <b>Bot Commands (Works Only In Groups) :</b>
 
-    -> <code>/add chat_id</code>
+    🔆 <code>/add chat_id</code>
                 OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
      <code>/add @Username</code>
      
-    -> <code>/del chat_id</code>
+    🔆 <code>/del chat_id</code>
                 OR                  - To disconnect A Group With A Channel
      <code>/del @Username</code>
      
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    🔆 <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+    🔆 <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
 
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            🔅 <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
             
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+            🔅 <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
 
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            🔅 <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
             
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            🔅 <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CINEHUB_family
+<b>🍀 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ; @cinehub_family</b>
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <b>🄲🄸🄽🄴🄷🅄🄱 Auto Filter</b>
